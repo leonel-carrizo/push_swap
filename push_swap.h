4e94ea097b04447cb6931570018b8922377d28d2
Leonel Ricardo Carrizo Marin <lcarrizo@c3r5s1.42london.com>
2024-02-13 15:13:00 +0000

    backup all

---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcarrizo <lcarrizo@student.42london.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 13:47:26 by lcarrizo          #+#    #+#             */
+/*   Updated: 2024/02/13 13:51:15 by lcarrizo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PUSH_SWAP_H
+# define FT_PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <limist.h>
+
+void	push_swap();
+
+
+#endif
